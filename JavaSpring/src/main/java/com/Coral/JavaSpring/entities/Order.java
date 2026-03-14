0d9c4997c04e871f3fd7d7cb535fc46b4d36632d
Luiz Coral <luizcoral012@gmail.com>
2026-03-14 18:54:38 -0300

    Enum OrderStatus

--- JavaSpring/src/main/java/com/Coral/JavaSpring/entities/Order.java
+++ JavaSpring/src/main/java/com/Coral/JavaSpring/entities/Order.java
@@ -1,5 +1,6 @@
 package com.Coral.JavaSpring.entities;
 
+import com.Coral.JavaSpring.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
@@ -19,6 +20,8 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
 
     @ManyToOne
     @JoinColumn(name = "cliente_id")
@@ -26,18 +29,11 @@ public class Order implements Serializable {
 
     public Order (){}
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
-    }
-
-    public Instant getMoment() {
-        return moment;
-    }
-
-    public void setMoment(Instant moment) {
-        this.moment = moment;
     }
 
     public User getClient() {
@@ -55,6 +51,26 @@ public class Order implements Serializable {
     public void setId(Long id) {
         this.id = id;
     }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null){
+        this.orderStatus = orderStatus.getCode();
+    }
+    }
+
+
 
     @Override
     public boolean equals(Object o) {
