8ce6bd542e05cd529b01b3dce80113303c3bcd23
Luiz Coral <luizcoral012@gmail.com>
2026-03-14 21:43:30 -0300

    Associação muitos para muitos entre Product e OrderItem

--- JavaSpring/src/main/java/com/Coral/JavaSpring/entities/Product.java
+++ JavaSpring/src/main/java/com/Coral/JavaSpring/entities/Product.java
@@ -20,12 +20,15 @@ public class Product implements Serializable {
     private Double price;
     private String imgUrl;
 
-    @JsonIgnore
+    //@JsonIgnore
     @ManyToMany
     @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
     public Product(){};
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product(Long id, String name, String description, Double price, String imgUrl) {
         this.id = id;
@@ -77,6 +80,15 @@ public class Product implements Serializable {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders() {
+        Set<Order> set = new HashSet<>();
+        for (OrderItem x : items) {
+            set.add(x.getOrder());
+        }
+        return set;
     }
 
     @Override
